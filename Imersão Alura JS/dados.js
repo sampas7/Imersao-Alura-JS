3d40dd2ff584cf5f1293e0bb58663fe42dcc6cbe
Daniel Sampaio <danielsampaiorocha@gmail.com>
2024-09-05 22:26:59 -0300

    Imersão - Aula 3

---
+++ Imersão Alura JS/dados.js
@@ -0,0 +1,19 @@
+let dados = [{
+    titulo : "Mixagem",
+    descricao : "Vamo fazer magia nos seus vocais e deixar seu som mais diferenciado? Cola comigo então!",
+    link : "https://youtu.be/Ur8Mx8H861E?si=iiOx8tfjAhNM6PIK&t=86"
+
+},
+{
+    titulo : "Masterização",
+    descricao : "Já fez a mixagem e precisa dar aquele up com a masterização? Eu faço a parada acontecer!",
+    link : "https://youtu.be/Ur8Mx8H861E?si=qOCgROcODJv1Cc18&t=167"
+
+}
+,
+{
+    titulo : "Beat",
+    descricao : "Tá precisando de um type beat pra lançar as barras e barras? Tô pronto pra fazer pra você!",
+    link : "https://youtu.be/bESsIi7ARVY"
+
+}];
