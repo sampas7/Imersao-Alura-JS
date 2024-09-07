5489b4885b571146ca9e89bb83005885e579c41f
Daniel Sampaio <danielsampaiorocha@gmail.com>
2024-09-06 21:51:12 -0300

    Imersão finalizada.

--- Imersão Alura JS/app.js
+++ Imersão Alura JS/app.js
@@ -1,1 +1,50 @@
-console.log(dados);
+function buscar() {
+    // Seleciona o elemento HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa")
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if(campoPesquisa == ""){
+        section.innerHTML = "<s1>Nada foi encontrado.</s1>"
+        return             
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = ""
+    let titulo = ""
+    let descricao = ""
+    let tags = ""
+
+    // Para cada item de dados, constrói o HTML dos resultados
+    for (let dado of dados) {
+
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="${dado.instagram}" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">${dado.definicao}</a>
+            </div>
+            `
+        }
+    }
+
+    // Caso não tenha encontrado nada, 
+    if (!resultados){
+        resultados = section.innerHTML = "<s1>Nada foi encontrado, o Sampas não fornece esse serviço.</s1>"       
+    }
+
+    // Atualiza o conteúdo do elemento HTML com os resultados construídos
+    section.innerHTML = resultados
+
+}
--- Imersão Alura JS/dados.js
+++ Imersão Alura JS/dados.js
@@ -1,19 +1,44 @@
 let dados = [{
     titulo : "Mixagem",
-    descricao : "Vamo fazer magia nos seus vocais e deixar seu som mais diferenciado? Cola comigo então!",
-    link : "https://youtu.be/Ur8Mx8H861E?si=iiOx8tfjAhNM6PIK&t=86"
+    instagram : "https://www.instagram.com/samprodx",
+    descricao : "Vamo fazer magia nos seus vocais e deixar seu som mais diferenciado? Cola comigo então, são os R$30,00 mais bem gastos da sua vida, o seu som vai ser tranformado em um verdadeiro espetáculo.",
+    link : "https://youtu.be/Ur8Mx8H861E?si=iJ5PODfs9q0hkY4Z&t=100",
+    definicao : "Afinal, o que é mixagem?",
+    tags : "delay reverb compressor fl studio efeitos" 
 
 },
 {
     titulo : "Masterização",
-    descricao : "Já fez a mixagem e precisa dar aquele up com a masterização? Eu faço a parada acontecer!",
-    link : "https://youtu.be/Ur8Mx8H861E?si=qOCgROcODJv1Cc18&t=167"
+    instagram : "https://www.instagram.com/samprodx",
+    descricao : "Já fez a mixagem e precisa dar aquele up com a masterização? Eu faço a parada acontecer! Por R$30,00 eu melhoro ainda mais seu trampo e deixo ele pronto pra ser enviado para as plataformas.",
+    link : "https://youtu.be/Ur8Mx8H861E?si=qOCgROcODJv1Cc18&t=167",
+    definicao : "E o que é masterização?",
+    tags : "limiter waves volume fl studio"
 
-}
-,
+},
+{
+    titulo : "Mix/Master",
+    instagram : "https://www.instagram.com/samprodx",
+    descricao : "Aqui é a junção dos 2 mundos, você captou os vocais e precisa da mixagem e masterização? Tu veio ao lugar certo, nesse combo os 2 serviços juntos estão saindo por R$50,00.",
+    link : "https://youtu.be/Ur8Mx8H861E?si=0wd975EvcrSfwtTL",
+    definicao : "Entendendo Mix e Master.",
+    tags : "pmm mixou mixar masterizar polir volume fl studio"
+
+},
 {
     titulo : "Beat",
-    descricao : "Tá precisando de um type beat pra lançar as barras e barras? Tô pronto pra fazer pra você!",
-    link : "https://youtu.be/bESsIi7ARVY"
+    instagram : "https://www.instagram.com/samprodx",
+    descricao : "Tá precisando de um type beat pra lançar as barras e barras? Tô pronto pra fazer pra você, e o melhor: no precinho, por apenas R$50,00, eu faço o beat que você precisa!",
+    link : "https://youtu.be/bESsIi7ARVY",
+    definicao : "O que é um type beat?",
+    tags : "808 grave kick hihat open hat jersey pluggnb trap rap boombap drill"
 
+},
+{
+    titulo: "Combo",
+    instagram: "https://www.instagram.com/samprodx",
+    descricao: "Quer ter seu som completo e profissa? Com meu combo, você adquire mixagem, masterização e um beat exclusivo, tudo isso por R$100,00. Esse é o pacote completo pra você lançar o hit do ano.",
+    link: "https://www.instagram.com/samprodx",
+    definicao: "A melhor opção para quem busca qualidade e praticidade.",
+    tags : "serviço tudo beatmaker produzir produtor pmm"
 }];
